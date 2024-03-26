62ca121bd1200ee4eeedec2fcaba9918bd72de54
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-03-26 11:06:26 -0300

    Add files via upload

---
+++ comparacoes.js
@@ -0,0 +1,17 @@
+//comparação implícita
+const numero = 5;
+const texto = '5'
+
+
+
+console.log(numero == texto)
+// comparação explícita
+
+console.log(numero === texto)
+//tipo de dados das variavéis 
+
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == compara o valor
+// === compara o valor e o tipo de dados
---
+++ console.js
@@ -0,0 +1,8 @@
+const minhavar = true;
+
+console.log(245)
+console.log("teste console")
+console.log(minhavar)
+
+//tratamento de erros
+console.error('Deu erro!!')
